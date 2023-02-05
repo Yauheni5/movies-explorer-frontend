b8ad8cf9e5b86e299065f431ad705a6eaf8f1aee
Yauheni5 <yauheni5@github.com>
2023-02-05 14:22:16 +0300

    feat: add focus input

--- src/components/Seacher/Seacher.js
+++ src/components/Seacher/Seacher.js
@@ -2,9 +2,18 @@ import { useEffect, useState } from "react"
 
 export default function Seacher() {
   const [shortFilm, setShortFilm] = useState(false);
-  function handleClickCheckBox(e) {
-    setShortFilm(!shortFilm);
+  const [inputFilter, setInputFilter] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(inputFilter);
+    setInputFilter("");
   }
+
+  function handleChangeFilterInput (e){
+    setInputFilter(e.target.value)
+  }
+
   useEffect (()=>{
     if(shortFilm) {
       console.log("включать короткометражки");
@@ -12,12 +21,13 @@ export default function Seacher() {
       console.log("не включать в поиск короткометражки");
     }
   },[shortFilm]);
+
   return (
     <section className="seacher section">
-      <form className="seacher__form">
-        <input className="seacher__input" type="text" placeholder="Фильм" minLength={2} maxLength={30} required/>
-        <button className="seacher__button" />
-          <div className="seacher__track" onClick={handleClickCheckBox}>
+      <form className="seacher__form" onSubmit={handleSubmit}>
+        <input className="seacher__input" onChange={handleChangeFilterInput} type="text" placeholder="Фильм" value={inputFilter} minLength={2} maxLength={30} required/>
+        <button className="seacher__button" type="submit" />
+          <div className="seacher__track" onClick={()=> setShortFilm(!shortFilm)}>
             <div className={shortFilm ? "seacher__thumb seacher__thumb_active" : "seacher__thumb"}></div>
             <p className="seacher__label">Короткометражки</p>
           </div>
